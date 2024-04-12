cffd4a897700c9a3cf4d03819e094d2ea85d7b82
deepankergoyal <deepankergoyal1@gmail.com>
2024-04-12 00:35:31 -0400

    final

--- app.js
+++ app.js
@@ -33,6 +33,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 require("dotenv").config();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 const DB = process.env.DATABASE;
 mongoose
   .connect(DB, {
